11540009e3cb6f09b17215149a4f83e55abdfdf1
enpitsulin <enpitsulin@gmail.com>
2023-10-13 09:28:10 +0800

    chore: use flag enable native esm support in test

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,3 @@
-/**
- * unified eco and its peer deps both are using pure ESM
- * so that need add to transformIngorePatterns array
- */
-const esmDepsPatterns = [
-  "property-information",
-  "direction",
-  "comma-separated-tokens",
-  "space-separated-tokens",
-  "zwitch",
-  "bcp-47-match",
-  "html-void-elements",
-  "micromark-util-character",
-  "stringify-entities",
-  "character-entities-legacy",
-  "character-entities-html4",
-  "ccount",
-  "fault",
-  "micromark-extension-frontmatter",
-  "decode-named-character-reference",
-  "character-entities",
-  "trim-lines",
-  "bail",
-  "is-plain-obj",
-  "trough",
-  "vfile",
-  "(.*)?(rehype|remark|micromark|unified)(.*)?",
-  "(.*)?(mdast|unist|hast)-util-(.*)(.*)?",
-];
-
 /** @type {import('jest').Config} */
 module.exports = {
   preset: "jest-expo",
@@ -37,6 +7,6 @@ module.exports = {
     "\\.[jt]sx?$": "babel-jest",
   },
   transformIgnorePatterns: [
-    `node_modules/(?!${esmDepsPatterns.join("|")}|((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)`,
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
 };
